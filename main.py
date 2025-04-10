41e3fa4a236b1c000499757d879940f9753696ba
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2025-04-10 18:22:01 +0400

    feat: add main pipline to scrape data as memory efficiently as possible

---
+++ main.py
@@ -0,0 +1,16 @@
+from scraper.pipeline import Pipeline
+"""main.py"""
+import asyncio
+
+async def main():
+    pipeline = Pipeline(
+        output_dir="data/output",
+        job_count=2,
+        local="ge",
+        batch_size=10,
+        max_concurrent_details=5
+    )
+    await pipeline.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
